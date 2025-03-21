857ac3c8f8ec245c379cdfd38b238cd4a2fe3431
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-03-21 17:25:28 +0530

    button variants initiated

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,9 @@
-"use client"
-import { Github } from "@/icons/Github"
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-
-
     return (
-        <div className="bg-black flex flex-col justify-center items-center text-white h-screen">
-            <div>
-                <img className="hover:scale-110 cursor-pointer transition-all duration-300" src="SWS.png" width="250" alt="swsLogo" />
-            </div>
-            <div className="md:text-5xl text-xl animate-bounce font-bold cursor-pointer">
-                Website Under Development
-            </div>
-            <div className="md:text-2xl text-sm text-blue-400 font-bold cursor-pointer text-center">
-            Join Our Courses to Master in Coding, Computer Science, Web Development, and in Tech & Beyond!
-
-            </div>
-            <div>
-                <div className="bg-blue-700 p-2 mt-8 font-bold flex justify-center space-x-2 rounded-full cursor-pointer transition-all duration-300 hover:scale-110">
-                    <div>
-                        <Github />
-                    </div>
-                    <div onClick={() => { window.open("https://github.com/studywithshubh") }}>
-                        OpenSource
-                    </div>
-                </div>
-            </div>
-        </div>
+        <>
+            Working on it!
+        </>
     )
 }
---
+++ components/ui/button.tsx
@@ -0,0 +1,38 @@
+import { ReactElement } from "react";
+
+const buttonVariants = {
+    general_1: "bg-white text-black px-6 font-bold py-1 rounded-md hover:bg-gray-200 hover:scale-105 transition-all duration-300",
+    general_2: "bg-black text-white px-6 font-bold py-1 rounded-md hover:bg-slate-800 hover:scale-105 transition-all duration-300",
+    blue_variant: "bg-blue-600 text-white font-bold hover:bg-blue-800 px-3 py-2 rounded-md transition-all duration-500 hover:scale-105",
+    red_variant: "bg-red-700 border-2 border-yellow text-white font-bold hover:bg-red-800 px-3 py-2 rounded-full transition-all duration-500 hover:scale-105",
+    purple_variant: "bg-purple-600 text-white font-bold hover:bg-purple-800 px-3 py-2 rounded-xl transition-all duration-500",
+}
+
+const defaultButtonStyles = "cursor-pointer flex items-center justify-center space-x-2";
+
+interface buttonProps {
+    variant: "general_1" | "general_2" | "blue_variant" | "red_variant" | "purple_variant";
+    text: string;
+    onClick?: () => void;
+    startIcon?: ReactElement;
+    endIcon?: ReactElement;
+}
+
+export const Button = ({variant , text , onClick , startIcon , endIcon}: buttonProps) => {
+    return (
+        <>
+            <button
+                className={`${buttonVariants[variant]} ${defaultButtonStyles}`}
+                onClick={onClick}
+            >
+                <div className="mr-2">
+                    {startIcon}
+                </div>
+                {text}
+                <div className="ml-2">
+                    {endIcon}
+                </div>
+            </button>
+        </>
+    )
+}
