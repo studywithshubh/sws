3d1662eb1de474161d7f53cacfb3f549a9843d4a
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-03-23 01:11:21 +0530

    connect fe to be testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 import { Navbar } from "@/components/Navbar";
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
     return (
-        <div className="">
+        <div>
             <Navbar/>
         </div>
     )
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,12 @@
-export const Navbar = () => {
+import axios from 'axios';
+
+export const Navbar = async () => {
+    const res = await axios.get("http://82.25.105.99/api/v1/auth/user/data");
+    
     return (
         <div className="flex justify-center">
             <div className="w-290 cursor-pointer h-36 border-2 mt-2 border-blue-300 rounded-2xl flex flex-col md:flex-row md:justify-around items-center shadow-sm shadow-blue-200 hover:shadow-lg hover:shadow-emerald-200 transition-all duration-500">
+                {JSON.stringify(res.data)}
             </div>
         </div>
     )
